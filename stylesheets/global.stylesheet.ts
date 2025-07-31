be55e5ded693b90ebf224cab6147feb333e3cf08
mrvkv <vivek69593@gmail.com>
2025-07-31 20:34:17 +0530

    stylesheet files seperated

---
+++ stylesheets/global.stylesheet.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 50,
+    },
+    button: {
+        marginTop: 20,
+        marginLeft: 50,
+        marginRight: 50,
+    },
+    modal: {
+        backgroundColor: "white",
+        marginLeft: 50,
+        marginRight: 50,
+        minHeight: 300,
+    },
+});
